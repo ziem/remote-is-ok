29d53d4446b6852ee3b1356e9556832d02af704d
ziem <zprzemek@vp.pl>
2021-07-29 21:06:28 +0200

    Add basic MainActivityTest

---
+++ app/src/androidTest/java/com/github/ziem/remoteisok/MainActivityTest.kt
@@ -0,0 +1,14 @@
+package com.github.ziem.remoteisok
+
+import androidx.test.core.app.launchActivity
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @Test
+    fun start() {
+        launchActivity<MainActivity>()
+    }
+}
